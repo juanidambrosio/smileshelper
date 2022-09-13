688c7671ff33bcbe7c416139fc729f7f7eb94baf
Juan D'Ambrosio <dambrosiojuani@gmail.com>
2022-09-13 20:18:11 +0200

    add usa, canada and france regions

--- config/constants.js
+++ config/constants.js
@@ -41,9 +41,17 @@ const COLOMBIA = ["BOG", "ADZ", "CTG", "SMR"];
 
 const CARIBE = ["CUN", "PTY", "PUJ", "SJO", "AUA", "HAV", "CTG"];
 
-const NAMERICA = ["MEX", "CHI", "NYC", "LAX", "DFW", "HNL", "SFO", "LAS"];
+const NAMERICA = ["MEX", "CHI", "NYC", "LAX", "DFW", "SFO", "LAS"];
+
+const USAESTE = ["NYC", "WAS", "PHL", "BOS", "DTT", "CHI"];
+
+const USAOESTE = ["LAX", "HNL", "SFO", "LAS", "SAN", "SMF"];
+
+const USASUR = ["DFW", "PHX", "IAH", "SAT", "ATL"];
 
 const FLORIDA = ["MIA", "FLL", "MCO", "TPA"];
+
+const CANADA = ["YTO", "YMQ", "YVR", "YOW", "YQB"];
 
 const EUROPA = ["LIS", "MAD", "BCN", "PAR", "AMS", "ROM", "LON", "FRA", "IST"];
 
@@ -52,6 +60,8 @@ const CEUROPA = ["BRU", "ATH", "BER", "ZRH", "VIE", "PRG"];
 const ESPANA = ["MAD", "BCN", "VLC", "PMI", "AGP", "IBZ", "SVQ"];
 
 const ITALIA = ["ROM", "MIL", "BLQ", "VCE", "NAP"];
+
+const FRANCIA = ["PAR", "MRS", "NCE", "LYS", "NTE", "TLS"];
 
 const ASIA = ["DXB", "BKK", "TLV", "TYO", "SEL", "DPS"];
 
@@ -75,10 +85,15 @@ const regions = {
   CARIBE,
   NAMERICA,
   FLORIDA,
+  USAESTE,
+  USAOESTE,
+  USASUR,
+  CANADA,
   EUROPA,
   CEUROPA,
   ESPANA,
   ITALIA,
+  FRANCIA,
   ASIA,
   MORIENTE,
   SASIA,

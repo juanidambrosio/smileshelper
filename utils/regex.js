a90d466f2da947c1535469fdcdaa274d98b180a0
Jorge <jorge@Jorges-MacBook-Air.local>
2022-09-15 13:41:43 -0300

    Arreglo de client. Cambio a filtros

--- utils/regex.js
+++ utils/regex.js
@@ -13,7 +13,7 @@ const regexRoundTrip =
   /^\w{3}(\s|-)\w{3}\s\d{4}(-|\/)[0-1]\d(-|\/)[0-3]\d(\s(\d|\w{3})){0,2} \d{4}(-|\/)[0-1]\d(-|\/)[0-3]\d\sm\d(\d)?(\sM\d(\d)?)?(\s(\d|\w{3})){0,2}$/;
 
 const regexAirlines =
-  /^\/parametros ((a:((\w{2}(\s|)){0,10})){0,1})((e:(\d)){0,1})$/i;
+  /^\/filtros ((a:((\w{2}(\s|)){0,10})){0,1})((e:(\d)){0,1})$/i;
 
 module.exports = {
   regexSingleCities,

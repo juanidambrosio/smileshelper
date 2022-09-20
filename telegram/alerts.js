1254e2f26fbf70c312cfa1020d6b722742f3f49b
Juan D'Ambrosio <dambrosiojuani@gmail.com>
2022-09-20 09:53:04 +0200

    add complete alert to telegram

--- telegram/alerts.js
+++ telegram/alerts.js
@@ -1,21 +1,32 @@
 const schedule = require("node-schedule");
 const { from, chat, texts } = require("../data/alertFlights");
 const { searchCityQuery } = require("./search");
+const { groupChatIdAlerts } = require("../config/constants");
+
+const rule = new schedule.RecurrenceRule();
+rule.hour = new schedule.Range(11, 23, 12);
 
 const checkDailyAlerts = async (bot) => {
-  schedule.scheduleJob(process.env.ALERTS_FREQUENCY, async () => {
+  schedule.scheduleJob(rule, async () => {
     for (const text of texts) {
       try {
         const { bestFlight, response } = await searchCityQuery({
           from,
           chat,
-          text,
+          text: text.journey,
+          promoMiles: text.promoMiles,
         });
-        console.log(bestFlight.price);
+        console.log(bestFlight.price + " " + new Date().toISOString());
+        if (bestFlight.price <= text.promoMiles) {
+          bot.sendMessage(
+            groupChatIdAlerts,
+            `ALERTA! Se encontró un tramo en promoción:\n ${response}`,
+            { parse_mode: "Markdown" }
+          );
+        }
       } catch (error) {
-        console.log(`Couldnt obtain result for alert query ${text}`);
+        console.log(`Couldnt obtain result for alert query ${text.journey}`);
       }
-      //bot.sendMessage(groupChatIdAlerts, "Alert Test!");
     }
   });
 };

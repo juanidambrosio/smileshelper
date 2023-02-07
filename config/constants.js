b48fcc23d12daf1aa73b2d41718eb02461da4954
unknown <dambrosiojuani@gmail.com>
2023-02-07 16:24:47 -0300

    enhance start command. add viva colombia as filterable

--- config/constants.js
+++ config/constants.js
@@ -6,8 +6,7 @@ const dailyTweet =
   "Te ayudamos a encontrar las 10 mejores ofertas en millas! Respondé este tweet con el formato [ORIGEN] [DESTINO] [AÑO-MES]. Ejemplo EZE MAD 2023-05.";
 
 const telegramStart =
-  "Te ayudamos a encontrar las mejores ofertas en millas \\! El bot soporta los siguientes ejemplos: \n\n __Búsqueda simple__:\n`EZE MAD 2023\\-05` \n\n __Búsqueda por tipo de cabina__:\n`EZE MAD 2023\\-05 EJE`\n`EZE MAD 2023\\-05 ECO`\n`EZE MAD 2023\\-05 PEC` \\(premium economy\\)\n\n __Búsqueda por cantidad de adultos__:\n`EZE MAD 2023\\-05 3`\n\n __Búsqueda por región__: \n`EZE EUROPA 2023\\-05`\n`EZE EUROPA 2023\\-05\\-01`\n`EUROPA EZE 2023\\-05\\-01`\n`EUROPA EZE 2023\\-05\\-01`\n\n__Búsqueda ida y vuelta__:\n`EZE BCN 2023\\-03\\-01 2023\\-03\\-30 m7`\n`EZE BCN 2023\\-03\\-01 2023\\-03\\-30 m7 M14`\n`EZE BCN 2023\\-03\\-01 EJE 2 2023\\-03\\-30 m7 ECO 3` \n\n __Parámetros ida y vuelta__:\n\\m\\[OBLIGATORIO\\]\\: Mínima cantidad de días entre ida y vuelta\nM \\[OPCIONAL\\]\\: Máxima cantidad de días entre ida y vuelta\n\n__Filtros configurables__:\na:\\[Códigos de aerolineas a EXCLUIR, separados por espacio\\]\n\\e:\\[Cantidad máxima de escalas del vuelo\\]\n\\r:\\[Cantidad máxima de vuelos mostrados en las búsquedas\\]\n\\h:\\[Cantidad máxima de horas de duración de los vuelos mostrados en las búsquedas\\]\n\\vf \\[Solo muestra los vuelos que aceptan Viaje Fácil\\]\nsingol \\[Muestra los vuelos desde/hacia Brasil que corresponden a otras aerolíneas que no sean Gol\\]\n\\smilesandmoney \\[Muestra los precios de los vuelos usando la tarifa de Smiles & Money\\]\n\n\\Ejemplo: /filtros a:AA IB e:1\n\\(en este ejemplo cualquier búsqueda realizada luego de configurar esos valores excluirá a American Airlines, a Iberia y buscará vuelos de 0 o 1 escala\\)\n\n\\Otros ejemplos \\(tener en cuenta que las aerolíneas a excluir se irán sumando a medida que utilice ese filtro\\): \n\\/filtros a:AR\n\\/filtros e:2\n\\/filtros r:15\n\\/filtros h:17\n\\/filtros vf\n\\/filtros singol\n\\/filtros a:AV e:0 r:30 vf\n\\\n\\Los códigos de las aerolíneas los puede encontrar usando el comando: /aerolineas\n\n\\Para consultar sus filtros puede usar el siguiente comando: \n\\/filtros\n\n\\Para eliminar sus filtros y que no sean considerados para las búsquedas puede usar el siguiente comando: \n\\/filtroseliminar";
-
+  "Te ayudamos a encontrar las mejores ofertas en millas \\! El bot soporta los siguientes ejemplos: \n\n __Búsqueda simple__:\n`EZE MAD 2023\\-05` \n\n __Búsqueda por tipo de cabina__:\n`EZE MAD 2023\\-05 EJE`\n`EZE MAD 2023\\-05 ECO`\n`EZE MAD 2023\\-05 PEC` \\(premium economy\\)\n\n __Búsqueda por cantidad de adultos__:\n`EZE MAD 2023\\-05 3`\n\n __Búsqueda por región__: \n`EZE EUROPA 2023\\-05`\n`EZE EUROPA 2023\\-05\\-01`\n`EUROPA EZE 2023\\-05\\-01`\n`EUROPA EZE 2023\\-05\\-01`\n\n__Búsqueda ida y vuelta__:\n`EZE BCN 2023\\-03\\-01 2023\\-03\\-30 m7`\n`EZE BCN 2023\\-03\\-01 2023\\-03\\-30 m7 M14`\n`EZE BCN 2023\\-03\\-01 EJE 2 2023\\-03\\-30 m7 ECO 3` \n\n __Parámetros ida y vuelta__:\n*m*\\[OBLIGATORIO\\]: Mínima cantidad de días entre ida y vuelta\n*M*\\[OPCIONAL\\]: Máxima cantidad de días entre ida y vuelta\n\n__Filtros configurables__:\n*a:*\\[Códigos de aerolineas a EXCLUIR, separados por espacio\\]\n*e:*\\[Cantidad máxima de escalas del vuelo\\]\n*r:*\\[Cantidad máxima de vuelos mostrados en las búsquedas\\]\n*h:*\\[Cantidad máxima de horas de duración de los vuelos mostrados en las búsquedas\\]\n*vf* \\[Solo muestra los vuelos que aceptan Viaje Fácil\\]\n*singol* \\[Muestra los vuelos desde/hacia Brasil que corresponden a otras aerolíneas que no sean Gol\\]\n*smilesandmoney* \\[Muestra los precios de los vuelos usando la tarifa de Smiles & Money\\]\n\nEjemplo:`/filtros a:AA IB e:1`\n\nEn el ejemplo anterior, cualquier búsqueda realizada luego de configurar esos valores excluirá a American Airlines, a Iberia y buscará vuelos de 0 o 1 escala\n\n__Otros Filtros__\n`/filtros a:AR`\n`/filtros e:2`\n`/filtros r:15`\n`/filtros h:17`\n`/filtros vf`\n`/filtros singol`\n`/filtros a:AV e:0 r:30 vf`\n\n__Consultar códigos de aerolineas__\n`/aerolineas`\n\n__Consultar Filtros__\n`/filtros`\n\n__Eliminar Filtros__\n`/filtroseliminar`\n\n__Nueva región__:\n`/nuevaregion <REGION> <AEROPUERTO1> <AEROPUERTO2> ... <AEROPUERTO10>`";
 const incorrectFormat =
   "El formato indicado es incorrecto. Intentá de nuevo respondiendo con [ORIGEN] [DESTINO] [AÑO]-[MES]";
 
@@ -38,7 +37,7 @@ const SMILES_EMISSION_URL = "https://www.smiles.com.ar/emission?";
 const SMILES_TAX_URL =
   "https://api-airlines-boarding-tax-prd.smiles.com.br/v1/airlines/flight";
 
-const airlinesCodes = `El listado de aerolíneas disponibles en Smiles es el siguiente: Use el código de 2 caracteres para configurar sus filtros:  \n\\\n\\AA: American Airlines\n\\AR: Aerolíneas Argentinas\n\\UX: Air Europa\n\\AM: AeroMéxico\n\\AV: Avianca\n\\CM: Copa Airlines\n\\AF: Air France\n\\KL: KLM\n\\AC: Air Canadá\n\\IB: Iberia\n\\EK: Emirates\n\\TK: Turkish Airlines\n\\TP: TAP Portugal\n\\SA: South African Airways\n\\ET: Ethiopian Airways\n\\AT: Royal Air Maroc\n\\KE: Korean Air\n\\2Z: Voe Pass\n\\G3: Gol\n\\FV: Viva Air\n\\A3: Aegean\n\\MS: Egyptair\n\\AS: Alaska Airlines\n\\EI: Aer Lingus\n\\VA: Virgin Australia\n\\V7: Volotea\n\\NH: Ana\n\\XW: Sky Express\n\\AI: Air India\n\\4O: Interjet\n\\OU: Croatia Airlines\n\\UP: Bahamas Air\n\\HA: Hawaiian Airlines\n\\OB: Boliviana de Aviación\n\\TR: Scoot\n\\OK: Czech Airlines\n\\JQ: Jetstar\n\\MN: Kulula\n\\PG: Bangkok Airways\n\\WM: Winair\n\\KQ: Kenya Airways\n\\BT: Air Baltic\n\\MU: China Estern\n\\ZP: Paranair\n\\ME: MEA\n\\GA: Garuda Indonesia\n\\HO: Juneyao Airlines\n\\SG: SpiceJet\n\\PY: Surinam Airways\n\\PS: UIA\n\\CX: Cathay Pacific\n\\TG: THAI\n\\JL: Japan Airlines\n\\S7: S7 Airlines\n\\FA: FlySafair\n\\H2: SKY Airline`;
+const airlinesCodes = `El listado de aerolíneas disponibles en Smiles es el siguiente: Use el código de 2 caracteres para configurar sus filtros:  \n\\\n\\AA: American Airlines\n\\AR: Aerolíneas Argentinas\n\\UX: Air Europa\n\\AM: AeroMéxico\n\\AV: Avianca\n\\CM: Copa Airlines\n\\AF: Air France\n\\KL: KLM\n\\AC: Air Canadá\n\\IB: Iberia\n\\EK: Emirates\n\\TK: Turkish Airlines\n\\TP: TAP Portugal\n\\SA: South African Airways\n\\ET: Ethiopian Airways\n\\AT: Royal Air Maroc\n\\KE: Korean Air\n\\2Z: Voe Pass\n\\G3: Gol\n\\VH: Viva Air\n\\A3: Aegean\n\\MS: Egyptair\n\\AS: Alaska Airlines\n\\EI: Aer Lingus\n\\VA: Virgin Australia\n\\V7: Volotea\n\\NH: Ana\n\\XW: Sky Express\n\\AI: Air India\n\\4O: Interjet\n\\OU: Croatia Airlines\n\\UP: Bahamas Air\n\\HA: Hawaiian Airlines\n\\OB: Boliviana de Aviación\n\\TR: Scoot\n\\OK: Czech Airlines\n\\JQ: Jetstar\n\\MN: Kulula\n\\PG: Bangkok Airways\n\\WM: Winair\n\\KQ: Kenya Airways\n\\BT: Air Baltic\n\\MU: China Estern\n\\ZP: Paranair\n\\ME: MEA\n\\GA: Garuda Indonesia\n\\HO: Juneyao Airlines\n\\SG: SpiceJet\n\\PY: Surinam Airways\n\\PS: UIA\n\\CX: Cathay Pacific\n\\TG: THAI\n\\JL: Japan Airlines\n\\S7: S7 Airlines\n\\FA: FlySafair\n\\H2: SKY Airline`;
 
 const preferencesSave = "Se han guardado sus filtros.";
 
@@ -59,7 +58,7 @@ const preferencesMap = new Map([
   ["smilesAndMoney", "smilesandmoney: "],
 ]);
 
-const regionSave = "Se ha guardado la nueva region."
+const regionSave = "Se ha guardado la nueva region.";
 
 const maxAirports = 10;
 
@@ -67,7 +66,6 @@ const tripTypes = {
   RETURN: "1",
   ONE_WAY: "2",
 };
-
 
 module.exports = {
   badResponse,
@@ -95,5 +93,5 @@ module.exports = {
   regionSave,
   airlinesCodes,
   maxAirports,
-  tripTypes
+  tripTypes,
 };

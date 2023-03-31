7c5e8e30007a71d1d3b172169159f919beb4deae
Juan D'Ambrosio <dambrosiojuani@gmail.com>
2023-03-31 20:50:50 +0200

    fix error when no SMILES_CLUB offer is included in alternatives

--- utils/calculate.js
+++ utils/calculate.js
@@ -10,34 +10,33 @@ const getBestFlight = (
     (previous, current) => {
       let currentMiles = Number.MAX_VALUE;
       if (flightSatisfiesPreferences(current, preferences)) {
-        currentFare = current.fareList.find(
-          (fare) => fare.type === offerType
-        );
-        currentMiles = currentFare.miles;
+        currentFare = current.fareList.find((fare) => fare.type === offerType);
+        currentMiles = currentFare?.miles || Number.MAX_VALUE;
       }
       return previous.price <= currentMiles
         ? previous
         : {
-          flight: current,
-          price: currentMiles,
-          money: currentFare.money,
-          fareUid: currentFare.uid,
-        };
+            flight: current,
+            price: currentMiles,
+            money: currentFare.money,
+            fareUid: currentFare.uid,
+          };
     },
     { flight: {}, price: Number.MAX_VALUE }
-  )
+  );
 };
 
 const flightSatisfiesPreferences = (flight, preferences) => {
   const { cabinType, airlines, stops, fare, maxhours } = preferences;
   const mappedCabinType = mapCabinType(cabinType);
 
-  return (mappedCabinType === "all" || flight.cabin === mappedCabinType) &&
+  return (
+    (mappedCabinType === "all" || flight.cabin === mappedCabinType) &&
     (!airlines || !airlines.includes(flight.airline.code)) &&
     (!stops || flight.stops <= stops) &&
     (!fare || flight.sourceFare === "AWARD") &&
     (!maxhours || Number(flight.duration.hours) <= Number(maxhours))
-
-}
+  );
+};
 
 module.exports = { getBestFlight };

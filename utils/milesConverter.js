57edd12f48e12a9e41aa316c9754d285c4aef3e2
Juan D'Ambrosio <dambrosiojuani@gmail.com>
2023-05-06 21:27:38 +0200

    little fix

--- utils/milesConverter.js
+++ utils/milesConverter.js
@@ -1,10 +1,8 @@
 const convertToMoney = (miles, taxPrice, milePrice, dolarPrice, moneyPrice) => {
-  const isSmilesAndMoneyPrice = moneyPrice !== "undefined";
-  const realMoneyPrice = isSmilesAndMoneyPrice ? parseFloat(moneyPrice) : 0;
   const arsPrice = parseFloat(
     parseInt(miles) * parseFloat(milePrice) +
       parseInt(taxPrice) +
-      realMoneyPrice
+      parseFloat(moneyPrice)
   );
   const usdPrice = arsPrice / parseInt(dolarPrice);
   return { arsPrice: arsPrice.toFixed(2), usdPrice: usdPrice.toFixed(2) };

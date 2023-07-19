b842f6fb4e736a453e824fba08aed3f79179d852
Juan D'Ambrosio <dambrosiojuani@gmail.com>
2023-07-19 21:39:38 +0200

    add endpoint for regional queries

--- aws/lambda/search.js
+++ aws/lambda/search.js
@@ -1,4 +1,8 @@
-const { getFlights } = require("../../clients/smilesClient.js");
+const {
+  getFlights,
+  getFlightsMultipleCities,
+  getFlightsRoundTrip,
+} = require("../../clients/smilesClient.js");
 
 const getMonthlyFlights = async (event) => {
   const { results, error, statusError } = await getFlights(
@@ -17,4 +21,50 @@ const getMonthlyFlights = async (event) => {
   }
 };
 
-module.exports = { getMonthlyFlights };
+const getRegionalFlights = async (event) => {
+  const { origin, destination, departureDate } = event.queryStringParameters;
+  const fixedDay = departureDate.length > 7;
+  if (origin.length > 3) {
+    isMultipleOrigin = true;
+    event.queryStringParameters.origin = origin.split(",");
+  } else {
+    isMultipleOrigin = false;
+    event.queryStringParameters.destination = destination.split(",");
+  }
+
+  const { results, error, statusError } = await getFlightsMultipleCities(
+    event.queryStringParameters,
+    fixedDay,
+    isMultipleOrigin
+  );
+  if (results) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    };
+  } else {
+    return {
+      statusCode: statusError,
+      error: error,
+    };
+  }
+};
+
+const getRoundTripFlights = async (event) => {
+  const { results, error, statusError } = await getFlightsRoundTrip(
+    event.queryStringParameters
+  );
+  if (results) {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results),
+    };
+  } else {
+    return {
+      statusCode: statusError,
+      error: error,
+    };
+  }
+};
+
+module.exports = { getMonthlyFlights, getRegionalFlights, getRoundTripFlights };

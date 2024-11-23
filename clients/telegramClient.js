6c4748be9559676cccfb48c33beae87cee169a9f
Juani D'Ambrosio <juanidambrosio@Juanis-MacBook-Air.local>
2024-11-23 19:32:18 +0100

    fix telegramclient

--- clients/telegramClient.js
+++ clients/telegramClient.js
@@ -6,8 +6,9 @@ const client = axios.create({
 
 const removePreviousMessages = async () => {
     const { data: { result } } = await client.get('/getUpdates')
+    if (!result?.length) return;
 
-    return await client.get(`/getUpdates?offset=${result[result.length - 1].update_id + 1}`)
+    return await client.get(`/getUpdates?offset=${result[result.length - 1].update_id + 1}`);
 }
 
 module.exports = {
